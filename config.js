e066f3cb8194feb76e4fd8b482dc77378977354e
ankitapkdeveloper <ankitapkdeveloper@gmail.com>
2026-09-09 08:31:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Get these from Supabase Dashboard → Project Settings → API
 window.SUPABASE_CONFIG = {
   url: "https://xubizzzuffhefxphzvxn.supabase.co",
-  anonKey: "PASTE_YOUR_PUBLISHABLE_OR_ANON_KEY_HERE"
+  anonKey: "sb_publishable_Ms62Fck5hKR5X7kqhF6tGg_lmw7U7yL"
 };
